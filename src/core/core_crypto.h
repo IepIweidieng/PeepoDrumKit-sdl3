b0945368b90bafd6079df25073a2ddf710421e0e
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2025-12-09 02:10:48 +0800

    Fix code

--- src/core/core_crypto.h
+++ src/core/core_crypto.h
@@ -4,12 +4,15 @@
 #include <vector>
 
 #define DEFINE_ENCRYPTION(key_str)                                                          \
+private:                                                                                    \
     static inline const std::vector<u8> EncryptionKey = plusaes::key_from_string(&key_str); \
-    static inline std::vector<u8> Decrypt(const u8 *data, size_t size)                      \
+                                                                                            \
+public:                                                                                     \
+    static std::vector<u8> Decrypt(const u8 *data, size_t size)                             \
     {                                                                                       \
         return DecryptData(data, size, EncryptionKey);                                      \
     }                                                                                       \
-    static inline std::vector<u8> Encrypt(const u8 *data, size_t size)                      \
+    static std::vector<u8> Encrypt(const u8 *data, size_t size)                             \
     {                                                                                       \
         return EncryptData(data, size, EncryptionKey);                                      \
     }
@@ -24,20 +27,14 @@ protected:
     // 压缩数据并加密，返回加密后的数据
     // 数据格式: [16字节IV][AES-256-CBC加密的gzip数据(PKCS7填充)]
     static std::vector<u8> EncryptData(const u8 *data, size_t size, const std::vector<u8> &key);
-
-public:
-    virtual std::vector<u8> Decrypt(const u8 *data, size_t size) = 0;
-    virtual std::vector<u8> Encrypt(const u8 *data, size_t size) = 0;
 };
 
 class EncryptedDataTable : public EncryptedFile
 {
-public:
     DEFINE_ENCRYPTION("500BB263557B418A951483FC2FFB15E4")
 };
 
 class EncryptedFumenV2 : public EncryptedFile
 {
-public:
     DEFINE_ENCRYPTION("D4B9F85708BC4C80038CDA2439751580")
 };
